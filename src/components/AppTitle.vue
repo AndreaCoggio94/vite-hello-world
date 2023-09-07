78fc5c12563471138d234df124bafc8f15039b57
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-07 16:38:25 +0200

    removed premade style and added message

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ message }}</h1>
+  </header>
+</template>
+
+<style lang="scss">
+h1 {
+  font-size: 3rem;
+}
+</style>
